0777d526341bbdfbbba76898847280a04e33540b
witold <wewolski@gmail.com>
2015-08-25 17:50:23 +0200

    fixing parameters

--- runSpecLRmd.R
+++ runSpecLRmd.R
@@ -1,18 +1,26 @@
 #! /usr/bin/env Rscript
 args <- commandArgs(TRUE)
-print(args)
+
 if (length(args)>=4){
   library(knitr)
   OUTPUTDIR <- args[1]
   WORKDIR <- args[2]
   FASTA_FILE <- args[3]
-  MAX_IONS <- args[4]
-  MIN_IONS <- args[5]
-  cat("Set params ",OUTPUTDIR , " ", WORKDIR, " ",FASTA_FILE , " ",MAX_IONS, " ", MIN_IONS, "\n"   )
+  MIN_IONS <- args[4]
+  MAX_IONS <- args[5]
+  
+  BLIB_REDUNDANT <- args[6]
+  BLIB_FILTERED <- args[7]
+  cat("Set params OUTPUTDIR = ",OUTPUTDIR ,
+      " WORKDIR = ", WORKDIR,
+      " FASTA_FILE = ",FASTA_FILE ,
+      " MAX_IONS = ",MAX_IONS, " MIN_IONS = ", MIN_IONS," BLIB_REDUNDANT = ",
+      BLIB_REDUNDANT, " BLIB_FILTERED = ",BLIB_FILTERED, "\n"   )
+  
   knit2pdf("specL.Rmd")
   
 }else{
-  print("runSpecLRmd.R OUTPUTDIR WORKDIR FASTA_FILE TOP_IONS MIN_IONS")
+  print("runSpecLRmd.R OUTPUTDIR INPUTDIR FASTA_FILE MIN_IONS MAX_IONS BLIB_FILTERED BLIB_REDUNDANT")
 }
 
 
