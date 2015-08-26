a9d264101196886c847c97b4662b2b2aa5356448
witold <wewolski@gmail.com>
2015-08-26 15:25:00 +0200

    optimizing params for rmarkdown::render

--- runSpecLRmd.R
+++ runSpecLRmd.R
@@ -16,7 +16,7 @@ if (length(args)>=4){
       " FASTA_FILE = ",FASTA_FILE ,
       " MAX_IONS = ",MAX_IONS, " MIN_IONS = ", MIN_IONS," BLIB_REDUNDANT = ",
       BLIB_REDUNDANT, " BLIB_FILTERED = ",BLIB_FILTERED, "\n"   )
-  rmarkdown::render("specL.Rmd",output_format ="pdf_document", output_file = file.path(OUTPUTDIR, "SpecL.pdf"))
+  rmarkdown::render("specL.Rmd",output_format ="pdf_document", output_file = file.path(OUTPUTDIR, "SpecL.pdf"), clean=FALSE)
 }else{
   print("runSpecLRmd.R OUTPUTDIR INPUTDIR FASTA_FILE MIN_IONS MAX_IONS BLIB_FILTERED BLIB_REDUNDANT")
 }
