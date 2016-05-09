dc7758304949901fcddafc7bc11c1b960b73beaa
wolski <wewolski@gmail.com>
2016-05-09 12:22:12 +0200

    debug

--- runspecl.sh
+++ runspecl.sh
@@ -1,1 +1,10 @@
 ./runSpecLRmd.R ./res.22348 ./temp.22348 /usr/local/mascot/sequence/fgcz_9606_d/current/fgcz_9606_d_20140506.fasta 0.8 5 6 blib.blib.db blib.blib.filtered.db
+
+
+ /scratch/wolski/generateSpecLibrary/runSpecLRmd.R /scratch/wolski/specL/res.49390 /scratch/wolski/specL/temp.49390 /usr/local/mascot/sequence/SwissProt/current/SwissProt_2016_02.fasta 0.05 5 6 blib.blib.db blib.blib.filtered.db
+
+ls /scratch/wolski/specL/temp.49390/
+
+
+/scratch/wolski/generateSpecLibrary/runSpecLRmd.R /scratch/wolski/specL/res.49530 /scratch/wolski/specL/temp.49530 /usr/local/mascot/sequence/SwissProt/current/SwissProt_2016_02.fasta 0.05 5 6 blib.blib.db blib.blib.filtered.db
+
