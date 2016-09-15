7782c6bc4fec1d8c66db5f2d4ed375fc6210757f
Witold Wolski <wewolski@gmail.com>
2016-09-15 15:08:38 +0200

    cleanup.

--- runSpecLRmd.R
+++ runSpecLRmd.R
@@ -12,11 +12,11 @@ if (length(args)>=4){
   BLIB_REDUNDANT <- args[7]
   BLIB_FILTERED <- args[8]
   cat("Set params OUTPUTDIR = ",OUTPUTDIR ,
-      " WORKDIR = ", WORKDIR,
-      " FASTA_FILE = ",FASTA_FILE , " MZ_ERROR = ", MZ_ERROR,
-      " MAX_IONS = ",MAX_IONS, " MIN_IONS = ", MIN_IONS," BLIB_REDUNDANT = ",
+      "\n WORKDIR = ", WORKDIR,
+      "\n FASTA_FILE = ",FASTA_FILE , " MZ_ERROR = ", MZ_ERROR,
+      "\n MAX_IONS = ",MAX_IONS, " MIN_IONS = ", MIN_IONS," BLIB_REDUNDANT = ",
       BLIB_REDUNDANT, " BLIB_FILTERED = ",BLIB_FILTERED, "\n")
-  rmarkdown::render("C:/users/wolski/prog/generateSpecLibrary/specL.Rmd",output_format ="pdf_document", output_file = file.path(OUTPUTDIR, "SpecL.pdf"), clean=FALSE)
+  rmarkdown::render("/scratch/wolski/generateSpecLibrary/specL.Rmd",output_format ="pdf_document", output_file = file.path(OUTPUTDIR, "SpecL.pdf"), clean=FALSE)
 }else{
   print("/scratch/wolski/generateSpecLibrary/runSpecLRmd.R OUTPUTDIR INPUTDIR FASTA_FILE MIN_IONS MAX_IONS BLIB_FILTERED BLIB_REDUNDANT")
   quit(status=1)
